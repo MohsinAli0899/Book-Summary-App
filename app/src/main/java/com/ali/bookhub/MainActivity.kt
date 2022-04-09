17186e54a4e488bffb5bc75a60dde88104dd744d
Mohsin0899 <mohsin.saifi98@gmail.com>
2022-04-09 23:26:56 +0530

    First Commit

--- app/src/main/java/com/ali/bookhub/MainActivity.kt
+++ app/src/main/java/com/ali/bookhub/MainActivity.kt
@@ -1,11 +1,55 @@
 package com.ali.bookhub
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuItem
+import android.widget.FrameLayout
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var coordinatorLayout: CoordinatorLayout
+    lateinit var toolbar: Toolbar
+    lateinit var frameLayout: FrameLayout
+    lateinit var navigationView: NavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+        coordinatorLayout = findViewById(R.id.coordinatorLayout)
+        toolbar = findViewById(R.id.toolbar)
+        frameLayout = findViewById(R.id.frame)
+        navigationView = findViewById(R.id.navigationView)
+        setUpToolbar()
+
+        val actionBarDrawerToggle = ActionBarDrawerToggle(
+            this@MainActivity, drawerLayout, R.string.open_drawer, R.string.close_drawer)
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)       //setting up hamburger icon
+        actionBarDrawerToggle.syncState()               //syncing opening closing state
+    }
+fun setUpToolbar(){
+    setSupportActionBar(toolbar)
+    supportActionBar?.title = "Toolbar Title"
+    supportActionBar?.setHomeButtonEnabled(true)        //added back button in toolbar
+    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+}
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        val id = item.itemId
+
+        if (id == android.R.id.home){
+            drawerLayout.openDrawer(GravityCompat.START)    //helps opening drawer from left side
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
