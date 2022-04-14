d101a089e0abb72f0e435acf3d3bd37735b81bf1
Mohsin0899 <mohsin.saifi98@gmail.com>
2022-04-14 23:11:35 +0530

    First COmmit

---
+++ app/src/main/java/com/ali/bookhub/fragment/DashboardFragment.kt
@@ -0,0 +1,27 @@
+package com.ali.bookhub.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.ali.bookhub.R
+
+class DashboardFragment : Fragment() {
+
+lateinit var recyclerDashboard: RecyclerView
+lateinit var layoutManager: RecyclerView.LayoutManager
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        val view = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        recyclerDashboard = view.findViewById(R.id.recyclerDashboard)
+        layoutManager = LinearLayoutManager(activity)
+        return view
+    }
+}
