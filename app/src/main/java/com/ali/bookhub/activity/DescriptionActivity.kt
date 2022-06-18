086dfbe473df1775307a95ef5b6e61ec6a6e1e6a
Mohsin0899 <mohsin.saifi98@gmail.com>
2022-06-18 21:00:20 +0530

    Changes

---
+++ app/src/main/java/com/ali/bookhub/activity/DescriptionActivity.kt
@@ -0,0 +1,12 @@
+package com.ali.bookhub.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ali.bookhub.R
+
+class DescriptionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_description)
+    }
+}
