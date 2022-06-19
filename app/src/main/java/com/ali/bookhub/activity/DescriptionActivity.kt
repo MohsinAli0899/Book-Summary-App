7023ea0f04e724c11ed9977ce0fe8fe799e66db3
Mohsin0899 <mohsin.saifi98@gmail.com>
2022-06-19 18:55:03 +0530

    Changes

--- app/src/main/java/com/ali/bookhub/activity/DescriptionActivity.kt
+++ app/src/main/java/com/ali/bookhub/activity/DescriptionActivity.kt
@@ -2,11 +2,37 @@ package com.ali.bookhub.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
 import com.ali.bookhub.R
 
 class DescriptionActivity : AppCompatActivity() {
+
+    lateinit var txtBookName: TextView
+    lateinit var txtBookAuthor: TextView
+    lateinit var txtBookPrice: TextView
+    lateinit var txtBookRating: TextView
+    lateinit var imgBookImage: ImageView
+    lateinit var txtBookDesc: TextView
+    lateinit var btnAddToFav: Button
+    lateinit var progressBar: ProgressBar
+    lateinit var progressLayout: RelativeLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_description)
+
+        txtBookName = findViewById(R.id.txtBookName)
+        txtBookAuthor = findViewById(R.id.txtBookAuthor)
+        txtBookPrice = findViewById(R.id.txtBookPrice)
+        txtBookRating = findViewById(R.id.txtBookRating)
+        imgBookImage = findViewById(R.id.imgBookImage)
+        txtBookDesc = findViewById(R.id.txtBookDesc)
+        btnAddToFav = findViewById(R.id.btnAddToFav)
+        progressBar = findViewById(R.id.progressBar)
+        progressBar.visibility = View.VISIBLE
+        progressLayout = findViewById(R.id.progressLayout)
+        progressLayout.visibility = View.VISIBLE
     }
+
 }
